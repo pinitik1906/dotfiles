242bdf3084aa0bba19305b6950f4e04fb59469b0
pinitik1906 <143661324+pinitik1906@users.noreply.github.com>
2024-09-09 00:26:59 +0800

    added opendoas as an alternative sudo

--- i3-void/.bashrc
+++ i3-void/.bashrc
@@ -10,13 +10,13 @@ alias grep='grep --color=auto'
 PS1='\[\e[1;31m\][\[\e[1;33m\]\u\[\e[1;32m\]@\[\e[1;34m\]\h \[\e[1;35m\]\w\[\e[1;31m\]]\[\e[1;00m\]\$\[\e[0;00m\] '
 
 alias lf="ranger"
-alias xir="sudo xbps-remove -Oofv"
-alias xii="sudo xbps-install -Suv"
+alias xir="doas xbps-remove -OoFv"
+alias xii="doas xbps-install -Suv"
 alias xiq="xbps-query -Rs"
 alias xif="xbps-query -f"
 alias yt-dlp="yt-dlp -S res:1080"
-alias reboot="sudo reboot"
-alias poweroff="sudo poweroff"
+alias reboot="doas reboot"
+alias poweroff="doas poweroff"
 alias rm="rm -rfv"
 alias bctl="bluetoothctl"
 
--- i3-void/install-musl-intel.sh
+++ i3-void/install-musl-intel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo xbps-install -Su elogind seatd polkit dbus xorg-minimal xss-lock i3 i3lock polybar noto-fonts-ttf noto-fonts-emoji fastfetch xorg-fonts ueberzug alacritty neovim mpv pipewire bluez bluez-utils libspa-bluetooth udisks2 ranger ufw pavucontrol dunst rofi rofi-calc rofi-emoji xclip xdotools xtools linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel brightnessctl feh zig gcc clang ffmpeg ffmpegthumbnailer
+sudo xbps-install -Su elogind seatd polkit dbus xorg-minimal xss-lock i3 i3lock polybar noto-fonts-ttf noto-fonts-emoji fastfetch xorg-fonts ueberzug alacritty neovim mpv pipewire bluez bluez-utils libspa-bluetooth udisks2 ranger ufw pavucontrol dunst rofi rofi-calc rofi-emoji xclip xdotools xtools linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel brightnessctl feh zig gcc clang ffmpeg ffmpegthumbnailer opendoas
 
 sudo ln -s /etc/sv/elogind /var/service/ && sudo ln -s /etc/sv/seatd /var/service && sudo ln -s /etc/sv/polkitd/ /var/service && sudo ln -s /etc/sv/dbus /var/service && sudo ln -s /etc/sv/ufw /var/service
 
 sudo mkdir -p /etc/pipewire/pipewire.conf.d && sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/ && sudo mkdir -p /etc/pipewire/pipewire.conf.d && sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
 sudo ufw limit 22/tcp && sudo ufw allow 80/tcp && sudo ufw allow 443/tcp && sudo ufw default deny incoming && sudo ufw default allow outgoing && sudo ufw enable
+
+echo "permit $(whoami) as root" | sudo tee -a /etc/doas.conf > /dev/null && echo "permit :wheel" | sudo tee -a /etc/doas.conf > /dev/null 
