05d16a954897f50358df3c36a219a07a64519686
pinitik1906 <143661324+pinitik1906@users.noreply.github.com>
2024-10-02 16:38:09 +0800

    cloning files from void to artix, and adding description to my install script

--- hypr-artix/install-artix-intel.sh
+++ hypr-artix/install-artix-intel.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# installing yay as a default AUR Helper
 sudo pacman -Syyu --needed base-devel git && git clone --depth 1 https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
+# enabling arch repos and installing hyprland
 yay -Syyu --needed artix-archlinux-support xorg hyprland dunst swaylock mpv nsxiv ranger zathura zathura-pdf-poppler bluez bluez-runit bluez-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel libnotify ufw hyprshot hyprpicker
 
+# ufw recommended settings
 sudo ufw limit 22/tcp && sudo ufw allow 80/tcp && sudo ufw allow 443/tcp && sudo ufw default deny incoming && sudo ufw default allow outgoing && sudo ufw enable
